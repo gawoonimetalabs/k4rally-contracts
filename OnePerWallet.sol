ccfb9628d960f1261cf49a72628349cede2a1efa
gawoonimetalabs <102616963+gawoonimetalabs@users.noreply.github.com>
2022-06-06 18:06:53 +0200

    Update OnePerWallet.sol

--- OnePerWallet.sol
+++ OnePerWallet.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-//import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "https://github.com/gawoonimetalabs/k4rally-contracts/blob/main/CheckAddress.sol";
 
 /// @author 1001.digital
